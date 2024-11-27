d169543d412ff35ad79685a8f8bef673c8253feb
roscanrares <roscanrares@gmail.com>
2024-11-28 00:22:55 +0200

    Third commit

--- config/db.php
+++ config/db.php
@@ -1,15 +1,16 @@
 <?php
-$host = 'localhost';
-$dbname = 'ong_management';
-$username = 'root';
-$password = 'Bobo2003';
+// Configurare pentru conexiunea MySQL
+$host = "ivgz2rnl5rh7spbh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"; // Hostul furnizat de JawsDB
+$port = "3306"; // Portul MySQL standard
+$dbname = "sdwylz8sv76m9gdq"; // Numele bazei de date
+$username = "f2w6a48j1tdjr087"; // Utilizatorul furnizat de JawsDB
+$password = "zv2sfr1ata791nk1"; // Parola furnizată de JawsDB
 
 try {
-    // $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $conn = new PDO("mysql://f2w6a48j1tdjr087:zv2sfr1ata791nk1@ivgz2rnl5rh7sphb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/sdwylz8sv76m9qdg");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
+    echo "Conexiunea la baza de date a fost realizată cu succes!";
 } catch (PDOException $e) {
     die("Conexiunea la baza de date a eșuat: " . $e->getMessage());
 }
+
 ?>
